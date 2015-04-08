a22839d3169489913484526326af0efe9978c388
Matt Gibbs <matt.gibbs@hp.com>
2015-04-08 13:45:45 -0400

    version 5.3.0.141

--- Source/nHydrate.Dsl/GeneratedCode/SerializationHelper.cs
+++ Source/nHydrate.Dsl/GeneratedCode/SerializationHelper.cs
@@ -1139,7 +1139,7 @@ namespace nHydrate.Dsl
 				// Only model has schema, diagram has no schema.
 				//rootElementSettings.SchemaTargetNamespace = "http://schemas.microsoft.com/dsltools/nHydrate";
 			}
-			rootElementSettings.Version = new global::System.Version("5.3.0.140");
+			rootElementSettings.Version = new global::System.Version("5.3.0.141");
 	
 			// Carry out the normal serialization.
 			rootSerializer.Write(serializationContext, rootElement, writer, rootElementSettings);
@@ -1161,7 +1161,7 @@ namespace nHydrate.Dsl
 				throw new global::System.ArgumentNullException("reader");
 			#endregion
 	
-			global::System.Version expectedVersion = new global::System.Version("5.3.0.140");
+			global::System.Version expectedVersion = new global::System.Version("5.3.0.141");
 			string dslVersionStr = reader.GetAttribute("dslVersion");
 			if (dslVersionStr != null)
 			{
