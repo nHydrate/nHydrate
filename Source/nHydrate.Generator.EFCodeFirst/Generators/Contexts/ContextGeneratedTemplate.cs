72d0ad9edfabbee329720790167dd0a1687581d2
Christopher Davis <chrisd@gravitybox.com>
2016-01-04 09:31:07 -0500

    removed the unnecessary configuration class re-versioned 5.3.0.157

--- Source/nHydrate.Generator.EFCodeFirst/Generators/Contexts/ContextGeneratedTemplate.cs
+++ Source/nHydrate.Generator.EFCodeFirst/Generators/Contexts/ContextGeneratedTemplate.cs
@@ -278,12 +278,6 @@ namespace nHydrate.Generator.EFCodeFirst.Generators.Contexts
             sb.AppendLine("		}");
             sb.AppendLine();
 
-            sb.AppendLine("		static " + _model.ProjectName + "Entities()");
-            sb.AppendLine("		{");
-            sb.AppendLine("			System.Data.Entity.DbConfiguration.SetConfiguration(new " + _model.ProjectName + "Configuration());");
-            sb.AppendLine("		}");
-            sb.AppendLine();
-
             #region Constructors
             sb.AppendLine("		#region Constructors");
             sb.AppendLine();
