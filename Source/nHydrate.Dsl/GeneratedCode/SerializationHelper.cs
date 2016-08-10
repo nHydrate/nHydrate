dd8935143ea1d8f55a351e78002ab049e103fbca
Christopher Davis <chrisd@gravitybox.com>
2016-08-10 09:49:56 -0400

    version 6.0.0.184

--- Source/nHydrate.Dsl/GeneratedCode/SerializationHelper.cs
+++ Source/nHydrate.Dsl/GeneratedCode/SerializationHelper.cs
@@ -1139,7 +1139,7 @@ namespace nHydrate.Dsl
 				// Only model has schema, diagram has no schema.
 				//rootElementSettings.SchemaTargetNamespace = "http://schemas.microsoft.com/dsltools/nHydrate";
 			}
-			rootElementSettings.Version = new global::System.Version("6.0.0.183");
+			rootElementSettings.Version = new global::System.Version("6.0.0.184");
 	
 			// Carry out the normal serialization.
 			rootSerializer.Write(serializationContext, rootElement, writer, rootElementSettings);
@@ -1161,7 +1161,7 @@ namespace nHydrate.Dsl
 				throw new global::System.ArgumentNullException("reader");
 			#endregion
 	
-			global::System.Version expectedVersion = new global::System.Version("6.0.0.183");
+			global::System.Version expectedVersion = new global::System.Version("6.0.0.184");
 			string dslVersionStr = reader.GetAttribute("dslVersion");
 			if (dslVersionStr != null)
 			{
