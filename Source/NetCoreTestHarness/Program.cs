ff0e49ba5001264019f90157e393b36a0bcf04ca
Chris Davis <chris.davis@onedatascan.com>
2020-08-21 07:31:33 -0400

    fix compile test issue

--- Source/NetCoreTestHarness/Program.cs
+++ Source/NetCoreTestHarness/Program.cs
@@ -8,7 +8,8 @@ namespace NetCoreTestHarness
         {
             var rootFolder = @"C:\code\nHydrateTestAug\ModelProject";
             var modelName = "Model1.nhydrate";
-            var model = FileManagement.Load(rootFolder, modelName);
+            bool wasLoaded;
+            var model = FileManagement.Load(rootFolder, modelName, out wasLoaded);
 
             var rootFolder2 = @"C:\Temp\ModelTest";
             FileManagement.Save(rootFolder2, modelName, model);
